eb7bcaa388ec7b9abfb52b4d45ef874b7dbc76c7
bot-lin <zlin6364@hotmail.com>
2024-03-27 10:20:52 +0800

    Update fleet_manager.py

--- rmf_demos_fleet_adapter/rmf_demos_fleet_adapter/fleet_manager.py
+++ rmf_demos_fleet_adapter/rmf_demos_fleet_adapter/fleet_manager.py
@@ -206,7 +206,7 @@ class FleetManager(Node):
                 state = self.robots.get(robot_name)
                 if state is None or state.state is None:
                     return response
-                self.get_logger().info("{} {} {}".format(state.location.x, state.location.y, state.location.yaw))
+                self.get_logger().info("{} {} {}".format(state.state.location.x, state.state.location.y, state.state.location.yaw))
                 response['data'] = self.get_robot_state(state, robot_name)
                 self.get_logger().info("{}".format(response['data']))
             response['success'] = True
