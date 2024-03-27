ff2531f9117819e1c13c0b1a2ad81805221ed395
bot-lin <zlin6364@hotmail.com>
2024-03-27 11:43:57 +0800

    Update fleet_manager.py

--- rmf_demos_fleet_adapter/rmf_demos_fleet_adapter/fleet_manager.py
+++ rmf_demos_fleet_adapter/rmf_demos_fleet_adapter/fleet_manager.py
@@ -417,6 +417,8 @@ class FleetManager(Node):
 
     def robot_state_cb(self, msg):
         self.get_logger().info("callback: {}".format(msg))
+        self.get_logger().info("self.robots: {}".format(self.robots))
+        self.get_logger().info("is msg.name in self.robots: {}".format(msg.name in self.robots))
         if msg.name in self.robots:
             robot = self.robots[msg.name]
             if (
